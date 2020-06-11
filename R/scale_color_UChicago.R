9cd443a48ec5614604a55e63e494869ae8199daa
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-06-11 14:07:53 -0500

    Initial commit

---
+++ R/scale_color_UChicago.R
@@ -0,0 +1,34 @@
+#' Change color to one of based on the University of Chicago's palettes
+#'
+#' @description Custom color scale to use the delabj palettes.
+#' @usage scale_color_UChicago(palette="main", reverse=FALSE, ...)
+#'
+#' @param palette Name of the palette (main, zune)
+#' @param reverse Boolean: should the palette be reversed
+scale_color_UChicago <- function(palette = 'core', discrete= TRUE, reverse = FALSE, ...){
+  pal <- get_UChicago_palettes(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    ggplot2::discrete_scale("colour", paste0("UChicago", palette), palette = pal, ...)
+  } else {
+    ggplot2::scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+#' Change fill to one of based on the University of Chicago's palettes
+#'
+#' @description Custom color scale to use the delabj palettes.
+#' @usage scale_fill_UChicago(palette="main", reverse=  F, ...)
+#'
+#' @param palette Name of the palette ("core", "shade", "tint")
+#' @param reverse Boolean: should the palette be reversed
+scale_fill_UChicago <- function(palette = 'core', discrete= TRUE, reverse = FALSE, ...){
+  pal <- get_UChicago_palettes(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    ggplot2::discrete_scale("fill", paste0("UChicago", palette), palette = pal, ...)
+  } else {
+    ggplot2::scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
